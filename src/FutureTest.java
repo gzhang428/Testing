dedb3decf352bfa87ee30ddc0cc636246d9aa4e6
Guannan <gzhang428@gmail.com>
2013-04-20 00:34:13 -0700

    over

--- src/FutureTest.java
+++ src/FutureTest.java
@@ -1,4 +1,10 @@
+import java.util.List;
+
 import org.apache.commons.lang3.StringUtils;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+
+import com.google.common.collect.Lists;
 
 
 public class FutureTest {
@@ -7,9 +13,11 @@ public class FutureTest {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		String[] str = {"a", "b"};
+		BasicConfigurator.configure();
+		Logger log = Logger.getLogger(FutureTest.class);
+		List<String> str = Lists.newArrayList("a", "b");
 		String string = StringUtils.join(str, "...");
-		System.out.println(string);
+		log.info(string);
 		
 	}
 
