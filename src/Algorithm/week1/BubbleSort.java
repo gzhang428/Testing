5ac85cc14584ace8e33db4f8c81d6ac12eb9c4a9
Guannan <gzhang428@gmail.com>
2014-05-04 19:07:22 -0700

    done

--- src/Algorithm/week1/BubbleSort.java
+++ src/Algorithm/week1/BubbleSort.java
@@ -3,32 +3,30 @@ public class BubbleSort {
 
 	public static void main(String[] args) {
 		int[] a = { 2, 1, 5, 7, 3, 8, 9, 0, 4, 6 };
+		int[] b = {9,8,7,6,5,4,3,2,1,0};
 		BubbleSort bubbleSort = new BubbleSort();
-		bubbleSort.sort(a);
+		bubbleSort.sort(b);
 
 	}
 
-	public void sort(int[] a) {
-		int j=0;
-		boolean swapped=true;
-		while(swapped){
-			j++;
-			swapped=false;
-			for(int i=0; i<a.length-j;i++){
-				if(a[i]>a[i+1]){
-					swap(i, i + 1, a);
-					swapped=true;
+	public void sort(int[] a){
+		for (int j = a.length - 1; j > 0; j--){
+			System.out.println(j);
+			boolean swapped = false;
+			for (int i = 0; i < j; i++){
+				System.out.println("i=" + i);
+				if (a[i] > a[i + 1]){
+					swap(a, i, i + 1);
+ 					swapped = true;
 				}
 			}
 			print(a);
+			if (!swapped){
+				break;
+			}
 		}
 	}
-
-	
-	public void sort2(int[] a){
-		
-	}
-	private void swap(int n, int m, int[] a) {
+	private void swap(int[] a, int n, int m ) {
 		int t = a[n];
 		a[n] = a[m];
 		a[m] = t;
--- src/Algorithm/week1/InsertionSort.java
+++ src/Algorithm/week1/InsertionSort.java
@@ -8,19 +8,25 @@ public class InsertionSort {
 
 	}
 
-
 	public void sort(int[] a) {
-		for (int j=1; j < a.length; j++){
-			int t=a[j];
+		for (int j = 1; j < a.length; j++){
 			int i;
-			for ( i=j; i >= 1 && a[i-1]>t; i--){
-				a[i]=a[i-1];
+			int t = a[j];
+			System.out.println("j=" + j + " t=" + t);
+			for (i = j; i > 0; i--){
+				if (a[i - 1] > t){
+					a[i] = a[i - 1];
+					System.out.println("i=" + i);
+					print(a);
+				} else{
+					break;
+				}
 			}
-			a[i]=t;
+			a[i] = t;
+			System.out.println("i=" + i);
+			print(a);
 		}
 	}
-
-
 	public void print(int[] a) {
 		for (int i = 0; i < a.length; i++) {
 			System.out.print(a[i]);
