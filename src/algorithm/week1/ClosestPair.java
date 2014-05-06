d7466867bb4df26fca7a0a8fc2a78829c3432764
Guannan <gzhang428@gmail.com>
2014-05-05 19:11:14 -0700

    strassen

---
+++ src/algorithm/week1/ClosestPair.java
@@ -0,0 +1,12 @@
+package algorithm.week1;
+
+public class ClosestPair {
+
+	public static void main(String[] args) {
+		int[][] ponits = {{1, 1}, 
+				          {1, 3}, 
+				          {2, 3}, 
+				          {4, 2}};
+	}
+
+}
